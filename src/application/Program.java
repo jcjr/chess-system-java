179e30ee71ab341cb9e778b8db0e7e5412a2c0a9
José Carlos Jr <josecarlosjr.atc@gmail.com>
2024-01-07 08:52:01 -0300

    Criado a lógica checkmate

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,7 +18,7 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		
-		while (true) {
+		while (!chessMatch.getcheckMate()) {
 			try {
 				
 				UI.clearScreen();
